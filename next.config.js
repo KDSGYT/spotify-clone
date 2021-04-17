f4163e658b9ab0c77e365d687418ec0dd8f9e31d
Karan Pal Singh <kdsgyt@gmail.com>
2021-04-17 15:19:30 -0400

    Basic setup of the app is done. Environment variables are now available

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    env: {
+        SPOTIFY_CLIENT_ID: process.env.NEXT_SPOTIFY_CLIENT_ID,
+        SPOTIFY_CLIENT_SECRET: process.env.NEXT_SPOTIFY_CLIENT_SECRET
+    }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,18 @@ import React from 'react';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [data, setData] = React.useState<String>()
   
+
+  // testing data fetching for local api
+  React.useEffect(() =>{
+    fetch('/api/hello')
+      .then((res) => res.json())
+      .then(data => data.name)
+      .then(name => setData(name))
+      
+  })
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,6 +24,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1>Somthing will go here</h1>
+        <h1>Data: {data}</h1>
       </main>
 
     </div>
