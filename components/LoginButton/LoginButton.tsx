92cebc4dff40ae7dde7c60402738018e81ce625f
Karan Pal Singh <kdsgyt@gmail.com>
2021-04-20 18:41:57 -0400

    Create a seperate component for the login button to

--- components/LoginButton/LoginButton.tsx
+++ components/LoginButton/LoginButton.tsx
@@ -13,7 +13,11 @@ const LoginButton = () => {
 
     const [authCode, setAuthCode] = React.useState<String>("");
 
-    let scopes = 'user-read-private user-read-email';
+    let scopes = ["user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state"].join(" ");
 
     let client_id = '050e648af66348d1b7bbaa86f5b64c94';
 
@@ -23,24 +27,26 @@ const LoginButton = () => {
         '?response_type=code' +
         '&client_id=' + client_id +
         (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-        '&redirect_uri=' + encodeURIComponent(redirect_uri)
+        '&redirect_uri=' + encodeURIComponent(redirect_uri) + '&show_dialog=true'
 
     // testing data fetching for local api
     React.useEffect(() => {
+        getAuthCode()
+    },[])
 
-        window.addEventListener('load', onAuthCode);
-
-        return () => {
-            window.removeEventListener('load', onAuthCode)
-        }
-
-    })
-
-    async function onAuthCode() {
+    async function getAuthCode() {
         const url = window.location.href;
 
         if (url.includes('code')) {
             const code = url.substr(url.indexOf('code=') + 5)
+            // const code = window.location.hash.substring(1).split('&').reduce((initial:any, item):any => {
+            //     let parts:any = item.split('=');
+            //     console.log(parts)
+            //     initial[parts[0]] = decodeURIComponent(parts[1]);
+            //     return initial;
+            // })
+            console.log(code)
+
             // setState
             await setAuthCode(code)
 
@@ -61,14 +67,6 @@ const LoginButton = () => {
      */
     function getAccessToken(code: String) {
 
-        const data = {
-            grant_type: "authorization_code",
-            code,
-            redirect_uri: process.env.NEXT_SPOTIFY_REDIRECT_URI,
-            client_id: process.env.NEXT_SPOTIFY_CLIENT_ID,
-            client_secret: process.env.NEXT_SPOTIFY_CLIENT_SECRET
-        }
-
         const tokenHeader: any = {
             'Content-Type': 'application/x-www-form-urlencoded'
         }
@@ -80,6 +78,7 @@ const LoginButton = () => {
             url: spotifyTokenURL,
             headers: {
                 'Authorization': 'Basic' + clientCredentials,
+                'Content-Type': 'application/x-www-form-urlencoded'
             },
             params: {
                 grant_type: "authorization_code",
@@ -87,8 +86,8 @@ const LoginButton = () => {
                 redirect_uri: redirect_uri,
             }
         })
-        .then((res: any) => console.log(res))
-        .catch((e: any) => console.log(clientCredentials))
+            .then((res: any) => console.log(res))
+            .catch((e: any) => console.log(clientCredentials))
     }
 
 
