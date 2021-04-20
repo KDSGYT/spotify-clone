7f3149a7e486f0aea346f35000325d7ce92683c4
Karan Pal Singh <kdsgyt@gmail.com>
2021-04-19 23:28:28 -0400

    Not able to access tokens ,  400 error

---
+++ CTX/CTX.tsx
@@ -0,0 +1,3 @@
+import React,{createContext} from 'react';
+
+const AuthCODECTX = createContext<String>("")
--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
+    // Environment Variables
     env: {
         SPOTIFY_CLIENT_ID: process.env.NEXT_SPOTIFY_CLIENT_ID,
-        SPOTIFY_CLIENT_SECRET: process.env.NEXT_SPOTIFY_CLIENT_SECRET
-    }
+        SPOTIFY_CLIENT_SECRET: process.env.NEXT_SPOTIFY_CLIENT_SECRET,
+        SPOTIFY_REDIRECT_URI: process.env.NEXT_SPOTIFY_REDIRECT_URI
+    },
 }
